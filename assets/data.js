531617138e05843a81a12165f07e0b6815ff0da5
Micaela Bahurlet <micabahurletgo@gmail.com>
2023-10-17 22:43:23 -0300

    Add data.Js and render all books

---
+++ assets/data.js
@@ -0,0 +1,65 @@
+const librosData =[
+    {
+        id: 1,
+        name: "Padre Rico, Padre Pobre",
+        bid: 7.000,
+        category: "Crecimiento",
+        img: "./img/PadreRicoPadrePobre.png"
+    },
+    { 
+        id: 2,
+        name: "Los Cuatro Acuerdos",
+        bid: 6.000,
+        category:"Crecimiento",
+        img: "./img/LosCuatroAcuerdos.png"
+    },
+    {
+        id: 3,
+        name: "¿Quién se ha llevado mi queso?",
+        bid: 5.000,
+        category: "Crecimiento",
+        img: "./img/QuienSeHaLlevadoMiQueso.png"
+    },
+    {
+        id: 4,
+        name: "1Q84",
+        bid: 6.000,
+        category: "Ficción",
+        img: "./img/HarukiMurakami.png"
+    },
+    {
+        id: 5,
+        name: "1984",
+        bid: 7.000,
+        category: "Ficción",
+        img: "./img/1984.png"
+    },
+    {
+        id: 6,
+        name: "Un Mundo Feliz",
+        bid: 6.000,
+        category: "Ficción",
+        img: "./img/UnMundoFeliz.png",
+    },
+    {
+        id: 7,
+        name: "Don Quijote de la Mancha",
+        bid: 10.000,
+        category: "Clásicos",
+        img: "./img/Quijote.png",
+    },
+    {
+        id: 8,
+        name: "Cien años de Soledad",
+        bid: 8.000,
+        category: "Clásicos",
+        img: "./img/CienAños.png",
+    },
+    {
+        id: 9,
+        name: "Ensayo sobre la ceguera",
+        bid: 7.000,
+        category: "Clásicos",
+        img: "./img/Ensayo.png",
+    }
+]
--- assets/main.js
+++ assets/main.js
@@ -12,3 +12,35 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+// -------------- Libros container ------------ //
+
+const librosContainer = document.querySelector(".librosContainer");
+
+// -------------- Function para crear el html de los libros ------------ //
+
+createProductTemplate = (libro) => {
+    return `
+        <div class="libroCard">
+            <img src="${libro.img}" alt="${libro.name}">
+            <h3>${libro.name}</h3>
+            <p>${libro.bid}€</p>
+            <button class="btn-categoria">Comprar</button>
+        </div>
+    `
+    
+}
+
+// -------------- Function renderLibros ------------ //
+
+const renderLibros = (librosList) => {
+    librosContainer.innerHTML = librosList.map(createProductTemplate).join("");
+}
+
+
+// -------------- Function innit------------ //
+const init = () => {
+    renderLibros(librosData);
+};
+
+init();
